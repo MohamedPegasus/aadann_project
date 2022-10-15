87e7f06d7ae076a9edb48924fd42c4f3f74a2fe8
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-15 19:44:56 +0200

    gh updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import {BrowserRouter  , Routes, Route} from 'react-router-dom';
 import Header from './components/header/Header';
 import Landing from './components/landing/Landing';
 import Footer from './components/footer/Footer'
@@ -15,20 +15,18 @@ function App() {
   return (
     <div className="App">
     <div className='gradient__bg'>
-    <Router>
+    <BrowserRouter>
     <Header/>
     < Landing />
-    <Routes>
+    
     <Route exact path='/' element={<Home />}></Route>
     <Route path='/Services'  element={<Services />}></Route>
     <Route path='/Contact'  element={<Contact />}></Route>
     <Route path='/Blog'  element={<Blog />}></Route>
     <Route path='/Login'  element={<Login />}></Route>
     <Route path='/Register'  element={<Register />}></Route>
-    </Routes>
-    
     <Footer />
-    </Router>
+    </BrowserRouter>
     </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <React.StrictMode>
+    <HashRouter>
     <App />
   </HashRouter>
+  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function

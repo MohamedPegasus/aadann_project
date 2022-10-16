60e2177e3fa434169cf66e92d220e5afb3cc9e0d
MohamedPegasus <mohamedheragy89@gmail.com>
2022-10-16 16:49:26 +0200

    changes by pk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter  , Routes , Route} from 'react-router-dom';
+import {BrowserRouter as Router , Routes as Switch, Route} from 'react-router-dom';
 import Header from './components/header/Header';
 import Landing from './components/landing/Landing';
 import Footer from './components/footer/Footer'
@@ -10,21 +10,23 @@ import Contact from './pages/Contact';
 import Blog from './pages/Blog';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <Header/>
     < Landing />
+    <Switch>
     <Route exact path='/aadann_project/' element={<Home />}></Route>
     <Route path='/aadann_project/Services'  element={<Services />}></Route>
     <Route path='/aadann_project/Contact'  element={<Contact />}></Route>
     <Route path='/aadann_project/Blog'  element={<Blog />}></Route>
     <Route path='/aadann_project/Login'  element={<Login />}></Route>
     <Route path='/aadann_project/Register'  element={<Register />}></Route>
+    </Switch>
     <Footer />
-    
-    </BrowserRouter>
+    </Router>
   );
 }
 
